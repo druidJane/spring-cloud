20b004346feacc186dff7398a05602af15e9420b
druid <413429011@qq.com>
2017-08-06 22:38:34 +0800

    e

--- api-gateway-dynamic-route/src/main/java/com/druid/ApiGatewayDynamicRouteApplication.java
+++ api-gateway-dynamic-route/src/main/java/com/druid/ApiGatewayDynamicRouteApplication.java
@@ -3,6 +3,7 @@ package com.druid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,7 @@ public class ApiGatewayDynamicRouteApplication {
 	}
 
 	@Bean
+	@RefreshScope
 	@ConfigurationProperties("zuul")
 	public ZuulProperties zuulProperties(){
 		return new ZuulProperties();
